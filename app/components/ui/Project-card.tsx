d49545292cc777e423ad5c8bf0999a4a225f39c2
Anghelo Alva <ance.anghelo@gmail.com>
2024-02-15 16:19:06 -0300

    Refactor ProjectCard component and add animation

--- app/components/ui/Project-card.tsx
+++ app/components/ui/Project-card.tsx
@@ -1,14 +1,30 @@
 import type { projects } from '@/app/lib/const/projects'
+import { motion, useInView } from 'framer-motion'
 import Image from 'next/image'
+import { useRef } from 'react'
+
 import { FaGithub, FaLink } from 'react-icons/fa6'
 
-export default function ProjectCard (
-  { project, index }: { project: typeof projects[0], index: number }
-): React.ReactElement {
+export default function ProjectCard ({
+  project, index
+}: ProjectCardProps): React.ReactElement {
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+
   const isEven = index % 2 === 0
+  const initialX = isEven ? -100 : 100
 
   return (
-    <div className={`flex flex-col items-center gap-2 md:items-start md:gap-5 mb-7 lg:mb-14 max-w-5xl ${isEven ? 'md:flex-row' : 'md:flex-row-reverse'}`}>
+    <motion.div
+      ref={ref}
+      initial={{ x: initialX, opacity: 0 }}
+      animate={isInView ? { x: 0, opacity: 1 } : {}}
+      transition={{ duration: 0.5 }}
+      className={`
+        flex flex-col items-center gap-2 md:items-start md:gap-5 mb-7 lg:mb-14 max-w-5xl
+        ${isEven ? 'md:flex-row' : 'md:flex-row-reverse'}
+      `}
+    >
       <div className={`${isEven ? 'md:text-end' : ''}`}>
         <div className={`w-full flex items-center justify-between ${isEven ? 'md:flex-row-reverse' : ''}`}>
           <h3 className="text-3xl font-bold text-start text-pink">
@@ -43,6 +59,11 @@ export default function ProjectCard (
         height={600}
         className='rounded-xl md:w-1/2'
       />
-    </div>
+    </motion.div>
   )
+}
+
+interface ProjectCardProps {
+  project: typeof projects[0]
+  index: number
 }
