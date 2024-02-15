71cddf9eb936efe72616438b24fdbcf50279fe01
Anghelo Alva <ance.anghelo@gmail.com>
2024-02-15 15:52:25 -0300

    Add scroll functionality to navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,8 @@
-'use client'
-
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
 export default function Navbar (): React.ReactElement {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const items = [
     { id: 'home', label: 'Home' },
     { id: 'skills', label: 'Skills' },
@@ -11,15 +10,52 @@ export default function Navbar (): React.ReactElement {
   ]
 
   const [selected, setSelected] = useState<string>('home')
-  const [hovered, setHovered] = useState<string>('home')
+  const [isInTransition, setIsInTransition] = useState<boolean>(false)
 
   const handleNavClick = useCallback((sectionId: string): void => {
+    setSelected(sectionId)
     const section = document.getElementById(sectionId)
+
     if (section != null) {
-      setSelected(sectionId)
+      setIsInTransition(true)
       section.scrollIntoView({ behavior: 'smooth' })
     }
   }, [setSelected])
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      let maxSection = ''
+      let maxSectionPosition = 0
+
+      if (isInTransition) {
+        return
+      }
+
+      items.forEach((item) => {
+        const section = document.getElementById(item.id)
+        if (section != null) {
+          const rect = section.getBoundingClientRect()
+          if (rect.top <= window.innerHeight / 10 && rect.bottom > window.innerHeight / 10) {
+            setSelected(item.id)
+          }
+          if (rect.bottom > maxSectionPosition) {
+            maxSectionPosition = rect.bottom
+            maxSection = item.id
+          }
+        }
+      })
+
+      if (maxSectionPosition < window.innerHeight / 2) {
+        setSelected(maxSection)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [items])
 
   return (
     <nav className="border border-white/10 py-1 rounded-full mb-12 fixed px-7 overflow-hidden top-7 md:top-10 z-50 backdrop-blur-xl left-1/2 transform -translate-x-1/2">
@@ -30,15 +66,21 @@ export default function Navbar (): React.ReactElement {
           return (
             <li
               key={item.id}
-              onClick={(): void => { handleNavClick(item.id) }}
-              onMouseOver={(): void => { setHovered(item.id) }}
-              onMouseLeave={(): void => { setHovered(selected) }}
+              onClick={(): void => {
+                handleNavClick(item.id)
+              }}
+              onMouseOver={(): void => {
+                setSelected(item.id)
+              }}
+              onMouseLeave={(): void => {
+                setSelected(selected)
+              }}
               data-active={isActive}
-              className='cursor-pointer px-4 py-2 rounded-lg lg:text-base relative no-underline duration-300 ease-in font-bold tracking-wider'
+              className="cursor-pointer px-4 py-2 rounded-lg lg:text-base relative no-underline duration-300 ease-in font-bold tracking-wider select-none"
             >
               <span>{item.label}</span>
 
-              {item.id === hovered && (
+              {(isActive) && (
                 <motion.div
                   className="absolute bottom-0 left-0 h-full bg-gradient-to-r rounded-lg -z-10 from-pink via-secondary-100 to-primary-100"
                   layoutId="navbar"
@@ -48,10 +90,10 @@ export default function Navbar (): React.ReactElement {
                   }}
                   transition={{
                     type: 'spring',
-                    bounce: 0.1,
-                    stiffness: 130,
+                    bounce: 1,
+                    stiffness: 100,
                     damping: 10,
-                    duration: 0.2
+                    duration: 1
                   }}
                 />
               )}
