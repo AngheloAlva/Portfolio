9b97c50224cebc9bdda2315358fd867d1fc98538
Anghelo Alva <ance.anghelo@gmail.com>
2024-03-30 16:30:25 -0300

    Update ESLint configuration and Navbar component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
+    extends: [
         "standard-with-typescript",
         "plugin:react/recommended",
-        "next/core-web-vitals"
+        "next/core-web-vitals",
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    plugins: ["react"],
+    rules: {},
+};
