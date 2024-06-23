0edee45114c3da0e38180df320cb5d3893364df2
Anghelo Alva <ance.anghelo@gmail.com>
2024-06-23 13:28:59 -0400

    Add bzConsulting experience

--- app/lib/const/experience.ts
+++ app/lib/const/experience.ts
@@ -1,7 +1,7 @@
 export const experience = [
   {
     title: 'IngSimple',
-    description: 'Development of the company\'s website, using Astro with islands and React components. Adding internationalization and a functional contact form achieved with Resend and Cloudflare Workers.',
+    description: 'Development of the company\'s website, using Astro with React islands components. Adding internationalization and a functional contact form achieved with Resend and Cloudflare Workers.',
     descriptionEs: 'Desarrollo de la página web de la empresa, utilizando Astro con islas y componentes de React. Agregando internacionalización y un formulario de contacto funcional logrado con Resend y Cloudflare Workers.',
     challenges: [
       'Learn how Cloudflare Workers and Pages work',
@@ -18,5 +18,23 @@ export const experience = [
     image: '/experience/ingsimple.webp',
     imageAlt: 'IngSimple website',
     url: 'https://ingsimple.cl'
+  },
+  {
+    title: 'BZ Consulting',
+    description: 'Development of the company\'s website, using Astro with React islands. This includes internationalization, a functional news page with MDX files, a link to documentation in Cloudflare R2, and a functional contact form achieved with Resend and Cloudflare Workers.',
+    descriptionEs: 'Desarrollo de la página web de la empresa, utilizando Astro con islas de React. Esta cuenta con internacionalización, una pagina de noticias funcional con archivos MDX, un elaze a documentacion en R2 de Cloudflare, un formulario de contacto funcional logrado con Resend y Cloudflare Workers.',
+    challenges: [
+      'Learn and apply the use of MDX files for the creation of news pages',
+      'Develop an attractive and functional user interface respecting the company\'s identity'
+    ],
+    challengesEs: [
+      'Aprender y aplicar el uso de archivos MDX para la creación de páginas de noticias',
+      'Desarrollar una interfaz de usuario atractiva y funcional respetando la identidad de la empresa'
+    ],
+    date: '2024 - Freelance',
+    techs: ['Astro', 'React', 'Tailwind CSS', 'TypeScript', 'Cloudflare Workers'],
+    image: '/experience/bzconsulting.png',
+    imageAlt: 'BZ Consulting website',
+    url: 'https://bzconsulting.cl'
   }
 ]
